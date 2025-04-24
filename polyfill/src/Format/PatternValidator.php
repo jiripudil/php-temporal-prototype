e3066ef92f84c14870a3bc81a94d31502aa26aea
Jiří Pudil <me@jiripudil.cz>
2025-04-24 20:45:37 +0200

    polyfill: update PatternValidator to match extension implementation

--- polyfill/src/Format/PatternValidator.php
+++ polyfill/src/Format/PatternValidator.php
@@ -24,37 +24,45 @@ final class PatternValidator
 
 	private const FIELDS = [
 		'G' => self::FIELD_DATE,
-		'u' => self::FIELD_DATE,
 		'y' => self::FIELD_DATE,
-		'D' => self::FIELD_DATE,
-		'M' => self::FIELD_DATE,
-		'L' => self::FIELD_DATE,
-		'd' => self::FIELD_DATE,
+		'Y' => self::FIELD_DATE,
+		'u' => self::FIELD_DATE,
+		'U' => self::FIELD_DATE,
+		'r' => self::FIELD_DATE,
 		'Q' => self::FIELD_DATE,
 		'q' => self::FIELD_DATE,
-		'Y' => self::FIELD_DATE,
+		'M' => self::FIELD_DATE,
+		'L' => self::FIELD_DATE,
+		'l' => self::FIELD_DATE,
 		'w' => self::FIELD_DATE,
 		'W' => self::FIELD_DATE,
+		'd' => self::FIELD_DATE,
+		'D' => self::FIELD_DATE,
+		'F' => self::FIELD_DATE,
+		'g' => self::FIELD_DATE,
 		'E' => self::FIELD_DATE,
 		'e' => self::FIELD_DATE,
 		'c' => self::FIELD_DATE,
-		'F' => self::FIELD_DATE,
+
 		'a' => self::FIELD_TIME,
+		'b' => self::FIELD_TIME,
+		'B' => self::FIELD_TIME,
 		'h' => self::FIELD_TIME,
-		'k' => self::FIELD_TIME,
-		'K' => self::FIELD_TIME,
 		'H' => self::FIELD_TIME,
+		'K' => self::FIELD_TIME,
+		'k' => self::FIELD_TIME,
 		'm' => self::FIELD_TIME,
 		's' => self::FIELD_TIME,
 		'S' => self::FIELD_TIME,
 		'A' => self::FIELD_TIME,
-		'n' => self::FIELD_TIME,
-		'N' => self::FIELD_TIME,
-		'V' => self::FIELD_TIMEZONE,
-		'x' => self::FIELD_TIMEZONE,
-		'X' => self::FIELD_TIMEZONE,
+
 		'z' => self::FIELD_TIMEZONE,
 		'Z' => self::FIELD_TIMEZONE,
+		'O' => self::FIELD_TIMEZONE,
+		'v' => self::FIELD_TIMEZONE,
+		'V' => self::FIELD_TIMEZONE,
+		'X' => self::FIELD_TIMEZONE,
+		'x' => self::FIELD_TIMEZONE,
 	];
 
 	private function __construct() {}
@@ -65,10 +73,6 @@ final class PatternValidator
 
 		$escaped = false;
 		foreach (str_split($pattern) as $char) {
-			if ($char === 'p') {
-				continue;
-			}
-
 			if ($char === '\'') {
 				$escaped = !$escaped;
 				continue;
@@ -78,7 +82,7 @@ final class PatternValidator
 				continue;
 			}
 
-			if (preg_match('/^\p{L}{1}$/u', $char) !== 1) {
+			if (preg_match('/^\p{L}$/u', $char) !== 1) {
 				continue;
 			}
 
